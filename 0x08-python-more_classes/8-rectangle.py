5e68af8a048dd56ccdd706f7b16261e767b4c8e0
Benjamin Taura <benjaminfondo01@gmail.com>
2021-12-14 03:22:51 -0800

    Compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# 7-rectangle.py
+# 8-rectangle.py
 # Benjamin Taura <1507@holbertonschool.com>
-""" File name : 7-rectangle.py
-    Change representation
+""" File name : 8-rectangle.py
+    Compare rectangles
     It is not allowed to import any module
 """
 
@@ -65,6 +65,17 @@ class Rectangle(object):
         if self.__width == 0 or self.__height == 0:
             return 0
         return ((2 * self.__width) + (2 * self.__height))
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Define bigger or equal between two rectangles"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
 
     def __str__(self):
         """Print the rectangle with the character #"""
