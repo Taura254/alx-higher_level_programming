8453ffa59189e45532ae3cb168e1fb6be52fe7db
Benjamin Taura <benjaminfondo01@gmail.com>
2021-12-14 01:55:11 -0800

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Benjamin Taura <1507@holbertonschool.com>
+""" File name : 0-rectangle.py
+    Simple rectangle : an empty class Rectangle that defines a rectangle
+    It is not allowed to import any module
+"""
+
+
+class Rectangle(object):
+    """Rectangle: Define new class"""
+    pass
