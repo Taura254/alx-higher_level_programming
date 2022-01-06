7cee63356b4c166f15d6bae720efdf52469537fe
Benjamin Taura <benjaminfondo01@gmail.com>
2022-01-06 03:17:24 -0800

    Geometry module

---
+++ 0x0A-python-inheritance/tests/5-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
