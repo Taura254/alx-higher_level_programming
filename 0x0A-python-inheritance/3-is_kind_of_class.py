263e84ae1ac167101878f9a6def191dd4ec43c85
Benjamin Taura <benjaminfondo01@gmail.com>
2022-01-06 11:32:11 -0800

    Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-# 2-is_same_class.py
+# 3-is_kind_of_class.py
 # Benjamin Taura <1507@holbertonschool.com>
-""" File name : 2-is_same_class.py
+""" File name : 3-is_kind_of_class.py
     It is not allowed to import any module
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """Define if a obj is instance of class"""
-    if type(obj) == a_class:
+    if isinstance(obj, a_class):
         return True
     return False
