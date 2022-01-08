591252aeab8c71cbe6ab806fba1267cd18504d0c
Benjamin Taura <benjaminfondo01@gmail.com>
2022-01-08 11:01:33 -0800

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-# 1-number_of_lines.py
-# Benjamin Taura <1507@holbertonschool.com>
-""" File name : 1-number_of_lines.py
-    Use the with statement
-    It is not allowed to import any module
-"""
+"""Number of lines module"""
 
 
 def number_of_lines(filename=""):
-    """number_of_lines: returns the number of lines of a text file:
-    Args:
-        filename (str): content of the file. Defaults to "".
-    """
-    with open(filename, 'r', encoding='utf-8') as f:
-        return len(f.readlines())
+    """Returns the number of lines in a text file"""
+    with open(filename, 'r') as o:
+        linum = 0
+        for line in o:
+            linum += 1
+        return linum
