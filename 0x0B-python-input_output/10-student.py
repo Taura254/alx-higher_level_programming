43652c933e7fb1b1fc14adc501a676e3c8cf4d3e
Benjamin Taura <benjaminfondo01@gmail.com>
2022-01-10 02:20:03 -0800

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" File name : 9-student.py
+""" File name : 11-student.py
 """
 
 
 class Student(object):
     """Class student"""
+
     def __init__(self, first_name, last_name, age):
         """__init__ initialized constructor
         Args:
@@ -16,6 +17,12 @@ class Student(object):
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """retrieves a dictionary represt """
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                dictionary[key] = value
+        return dictionary
