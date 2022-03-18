58676d077e170894b912e14ccc202b3206c44246
Taura254 <benjaminfondo01@gmail.com>
2022-03-18 05:25:28 -0700

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Error code #0"""
+"""Fetches https://intranet.hbtn.io/status."""
 import requests
 
+
 if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
-    req = "Body response:\n\t- type: {}\n\t- content: {}"
-    print(req.format(type(response.text), response.text))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
